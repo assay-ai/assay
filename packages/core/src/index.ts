585de6907bd00824b3e237fcb7054f53e7f1a7a9
aissam irhir <aissam@flexiapps.net>
2026-03-10 13:34:53 +0000

    feat(core): add public API barrel export

---
+++ packages/core/src/index.ts
@@ -0,0 +1,67 @@
+// Core types
+export type {
+  ToolCall,
+  LLMTestCase,
+  ConversationalTestCase,
+} from "./test-case.js";
+
+// Dataset
+export type { Golden, EvaluationDataset } from "./dataset.js";
+
+// Metric base
+export { BaseMetric } from "./metric.js";
+export type { MetricResult, MetricConfig } from "./metric.js";
+
+// Metrics
+export {
+  AnswerRelevancyMetric,
+  FaithfulnessMetric,
+  HallucinationMetric,
+  ContextualPrecisionMetric,
+  ContextualRecallMetric,
+  ContextualRelevancyMetric,
+  BiasMetric,
+  ToxicityMetric,
+  GEval,
+  SummarizationMetric,
+  ExactMatchMetric,
+  JsonCorrectnessMetric,
+} from "./metrics/index.js";
+export type {
+  GEvalConfig,
+  ExactMatchConfig,
+  JsonCorrectnessConfig,
+} from "./metrics/index.js";
+
+// Providers
+export {
+  BaseLLMProvider,
+  OpenAIProvider,
+  AnthropicProvider,
+  OllamaProvider,
+  resolveProvider,
+} from "./providers/index.js";
+export type { ProviderConfig } from "./providers/index.js";
+
+// Config
+export { resolveConfig, resetConfigCache } from "./config.js";
+export type { AssayConfig } from "./config.js";
+
+// Evaluate
+export { evaluate } from "./evaluate.js";
+export type { EvaluateConfig, EvaluateResult } from "./evaluate.js";
+
+// Assert
+export { assertEval } from "./assert.js";
+export type { AssertEvalOptions, AssertEvalResult } from "./assert.js";
+
+// Reporter
+export { ConsoleReporter } from "./reporter.js";
+export type { EvaluationSummary, TestCaseResult } from "./reporter.js";
+
+// Utilities
+export { parseJson, tryParseJson } from "./utils/json-parser.js";
+export type { ParseJsonOptions } from "./utils/json-parser.js";
+export { createLimiter } from "./utils/concurrency.js";
+export type { Limiter } from "./utils/concurrency.js";
+export { ratio, weightedAverage, meanAveragePrecision } from "./utils/scoring.js";
